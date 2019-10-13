2e5f71da9bf053857b81c2b1a3560c78ba8482cb
lixiaodong <961510699@qq.com>
2019-10-13 21:01:41 +0800

    完成了页面编辑修改的信息

--- web_back/tool/config.js
+++ web_back/tool/config.js
@@ -1,7 +1,12 @@
-// 设置基准路径  便于维护
-var URL = 'http://localhost:8000';
-// 用于存储每个接口的地址
-var USER_LOGIN = URL + '/admin/login';
-var USER_LOGIN_OUT = URL + '/admin/logout';
-var USER_INFO_GET = URL + '/admin/userinfo_get'
+// 这个文件中用来保存每个页面中使用的数据信息，例如接口地址
+// - baseUrl中保存的是基地址
+var baseUrl = 'http://localhost:8000';
+
+
+// - 后面的变量中保存的是每个接口的地址
+var USER_LOGIN = baseUrl + '/admin/login'; // 登录页登录接口
+var USER_LOGOUT = baseUrl + '/admin/logout'; // 首页退出登录接口
+var USER_INFO = baseUrl + '/admin/getuser'; // 首页用户信息获取接口(简单数据2条)
+var USER_INFO_GET = baseUrl + '/admin/userinfo_get'; // 用户页面详细信息获取(5条)
+var USER_INFO_EDIT = baseUrl + '/admin/userinfo_edit'; // 用户页面编辑信息
 
--- web_back/tool/user.js
+++ web_back/tool/user.js
@@ -1,27 +1,58 @@
-// 有许多的功能,请求
+// 用户可能存在多种请求的功能：登录、退出、获取信息。。。
 var user = {
-    login:function(options){
-        $.ajax({
-            type:'post',
-            url:USER_LOGIN,
-            data:options.data,
-            success:options.callback
-        })
+    // login函数用来进行登录接口的调用设置：
+    login: function (options) {
+      $.ajax({
+        type: 'post',
+        url: USER_LOGIN, // url地址是从config.js中获取的，要学会习惯
+        // 用户传入的请求参数
+        data: options.data,
+        // 用户传入的回调函数
+        success: options.callback
+        /* 
+        // 上面的写法是这种写法的简写：
+        success: function (res) {
+          options.callback(res);
+        } 
+        */
+      });
     },
-    // 退出功能
-    logout:function(options){
-        $.ajax({
-            type:'post',
-            url:USER_LOGIN_OUT,
-            success:options.callback
-        })
+    // logout函数用来进行退出接口的调用设置：
+    logout: function (options) {
+      $.ajax({
+        type: 'post',
+        url: USER_LOGOUT,
+        success: options.callback
+      })
     },
-    user_info_get:function(options){
-        $.ajax({
-            type:'get',
-            url:USER_INFO_GET,
-            success:options.callback
-        })
-
+  
+    // getInfo函数用来进行用户的信息获取接口设置：(简单数据2条)
+    getInfo: function (options) {
+      $.ajax({
+        url: USER_INFO,
+        success: options.callback
+      });
+    },
+  
+    // info_get函数用来获取用户的详细信息(5条)
+    info_get: function (options) {
+      $.ajax({
+        url: USER_INFO_GET,
+        success: options.callback
+      });
+    },
+  
+    info_edit: function (options) {
+      $.ajax({
+        type: 'post',
+        url: USER_INFO_EDIT,
+        data: options.data, // 请求发送的是formData
+        contentType: false,
+        processData: false,
+        success: options.callback
+      });
     }
-}
+  
+  
+  
+  };
